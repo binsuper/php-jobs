ebd618eb8353ab62e9017d34edcfdebab66187e3
huangyaobin <huangyaobin@piaoliusan.com>
2019-02-25 16:12:59 +0800

    update

--- console.php
+++ console.php
@@ -2,11 +2,15 @@
 
 define('GINO_JOBS_ROOT_PATH', __DIR__);
 
+if (!defined('GINO_JOBS_CONFIG_PATH')) {
+    define('GINO_JOBS_CONFIG_PATH', GINO_JOBS_ROOT_PATH . '/conf/config.php');
+}
+
 
 require_once GINO_JOBS_ROOT_PATH . '/vendor/autoload.php';
 require_once GINO_JOBS_ROOT_PATH . '/app/function.php';
 
-$config = include(GINO_JOBS_ROOT_PATH . '/conf/config.php');
+$config = include(GINO_JOBS_CONFIG_PATH);
 
 $console = new Gino\Jobs\Console($config);
 $console->run();
